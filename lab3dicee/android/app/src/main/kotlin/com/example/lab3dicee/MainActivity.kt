34f5a4899026e5addeaa4815ffe724902d34af58
utthi103 <truongthiutthi2003@gmail.com>
2024-11-07 15:07:25 +0700

    change

--- lab3dicee/android/app/src/main/kotlin/com/example/lab3dicee/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.lab3dicee
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lab4magicball/android/app/src/main/kotlin/com/example/lab4magicball/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.lab4magicball
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
